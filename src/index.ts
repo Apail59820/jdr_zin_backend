fc37bdc5520e1c6f9ae13158247f3adf08c1f5a1
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2024-10-20 01:21:17 +0200

    better code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import LoggerSingleton from "./utils/winston";
 
-import userRoutes from './routes/userRoutes';
+import userRoutes from "./routes/userRoutes";
+const chalk = require('chalk')
 
 const app = express();
 const port = 8055;
@@ -15,12 +16,24 @@ app.use(cookieParser());
 const logger = LoggerSingleton.getLogger();
 
 app.use((req, res, next) => {
-    logger.info(`${req.method} ${req.url}`);
-    next();
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    const statusCode = res.statusCode;
+
+    const statusColor = chalk.hex("#9400D3");
+
+    logger.info(
+      `${req.method} ${req.url} ${statusColor(statusCode)} - ${duration}ms`,
+    );
+  });
+
+  next(); // Continue to the next middleware
 });
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
-    logger.info(`Server started on port ${port}`);
+  logger.info(`Server started on port ${port}`);
 });
--- src/services/__tests__/userService.test.ts
+++ src/services/__tests__/userService.test.ts
@@ -7,11 +7,9 @@ jest.mock("@prisma/client", () => {
     PrismaClient: jest.fn().mockImplementation(() => {
       return {
         user: {
-          create: jest.fn().mockResolvedValue({ id: 1, name: "John Doe" }),
-          findUnique: jest.fn().mockResolvedValue({ id: 1, name: "John Doe" }),
-          findFirst: jest
-            .fn()
-            .mockResolvedValue({ id: 1, name: "John Doe", isActiveUser: true }),
+          create: jest.fn(),
+          findUnique: jest.fn(),
+          findFirst: jest.fn(),
         },
       };
     }),
@@ -22,67 +20,83 @@ const prisma = new PrismaClient();
 const userRepository = new UserRepository(prisma);
 const userService = new UserService(userRepository);
 
+const mockUser = { id: 1, name: "John Doe", isActiveUser: true };
+
 describe("UserService", () => {
+  beforeEach(() => {
+    // Resetting the default mock behavior before each test
+    (prisma.user.create as jest.Mock).mockResolvedValue(mockUser);
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(mockUser);
+    (prisma.user.findFirst as jest.Mock).mockResolvedValue(mockUser);
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should create a user successfully", async () => {
-    const userData = { name: "John Doe" };
-    const newUser = await userService.createUser(userData);
-    expect(newUser).toEqual({ id: 1, name: "John Doe" });
+  describe("createUser", () => {
+    it("should create a user successfully", async () => {
+      const userData = { name: "John Doe" };
+      const newUser = await userService.createUser(userData);
+      expect(newUser).toEqual(mockUser);
+    });
+
+    it("should handle error when creating user fails", async () => {
+      (prisma.user.create as jest.Mock).mockRejectedValueOnce(
+        new Error("Failed to create user"),
+      );
+
+      await expect(
+        userService.createUser({ name: "Jane Doe" }),
+      ).rejects.toThrow("Failed to create user");
+    });
   });
 
-  it("should get a user by id successfully", async () => {
+  describe("getUserById", () => {
     const userId = 1;
-    const user = await userService.getUserById(userId);
-    expect(user).toEqual({ id: 1, name: "John Doe" });
+
+    it("should get a user by id successfully", async () => {
+      const user = await userService.getUserById(userId);
+      expect(user).toEqual(mockUser);
+    });
+
+    it("should return null if user not found", async () => {
+      (prisma.user.findUnique as jest.Mock).mockResolvedValueOnce(null);
+      const user = await userService.getUserById(999);
+      expect(user).toBeNull();
+    });
+
+    it("should handle error when getting user by id fails", async () => {
+      (prisma.user.findUnique as jest.Mock).mockRejectedValueOnce(
+        new Error("Failed to get user"),
+      );
+
+      await expect(userService.getUserById(1)).rejects.toThrow(
+        "Failed to get user",
+      );
+    });
   });
 
-  it("should return null if user not found", async () => {
-    (prisma.user.findUnique as jest.Mock).mockResolvedValueOnce(null);
-    const user = await userService.getUserById(999);
-    expect(user).toBeNull();
-  });
+  describe("getActiveUser", () => {
+    it("should get the active user successfully", async () => {
+      const activeUser = await userService.getActiveUser();
+      expect(activeUser).toEqual(mockUser);
+    });
 
-  it("should get the active user successfully", async () => {
-    const activeUser = await userService.getActiveUser();
-    expect(activeUser).toEqual({ id: 1, name: "John Doe", isActiveUser: true });
-  });
+    it("should return null if no active user found", async () => {
+      (prisma.user.findFirst as jest.Mock).mockResolvedValueOnce(null);
+      const activeUser = await userService.getActiveUser();
+      expect(activeUser).toBeNull();
+    });
 
-  it("should return null if no active user found", async () => {
-    (prisma.user.findFirst as jest.Mock).mockResolvedValueOnce(null);
-    const activeUser = await userService.getActiveUser();
-    expect(activeUser).toBeNull();
-  });
+    it("should handle error when getting active user fails", async () => {
+      (prisma.user.findFirst as jest.Mock).mockRejectedValueOnce(
+        new Error("Failed to get active user"),
+      );
 
-  it("should handle error when creating user fails", async () => {
-    (prisma.user.create as jest.Mock).mockRejectedValueOnce(
-      new Error("Failed to create user"),
-    );
-
-    await expect(userService.createUser({ name: "Jane Doe" })).rejects.toThrow(
-      "Failed to create user",
-    );
-  });
-
-  it("should handle error when getting user by id fails", async () => {
-    (prisma.user.findUnique as jest.Mock).mockRejectedValueOnce(
-      new Error("Failed to get user"),
-    );
-
-    await expect(userService.getUserById(1)).rejects.toThrow(
-      "Failed to get user",
-    );
-  });
-
-  it("should handle error when getting active user fails", async () => {
-    (prisma.user.findFirst as jest.Mock).mockRejectedValueOnce(
-      new Error("Failed to get active user"),
-    );
-
-    await expect(userService.getActiveUser()).rejects.toThrow(
-      "Failed to get active user",
-    );
+      await expect(userService.getActiveUser()).rejects.toThrow(
+        "Failed to get active user",
+      );
+    });
   });
 });
